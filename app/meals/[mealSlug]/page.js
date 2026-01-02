cdb46fbac9aef4248493d7a52a9a7f049df74087
Ankit Srivastava <ankitsrivastava0103@gmail.com>
2026-01-02 19:41:52 +0530

    Header Component & Image Component of NextJs

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,3 @@
+export default function MealDetailsPage() {
+  return <h2>Meals Details Page</h2>;
+}
--- app/meals/[slug]/page.js
+++
@@ -1,3 +0,0 @@
-export default function MealsSlugPage() {
-  return <h2>Meals Slug Page</h2>;
-}
